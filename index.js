8c2b8ba013d51f19f49d5635182eb06fabb8d712
MulfordnSons <mmulford94@me.com>
2022-09-14 22:20:23 -0400

    logic is broken, fixing

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-function getCpuChoice () {
+userChoice = prompt('rock paper or scissors')
+
+computerChoice = function getCpuChoice () {
     let choices = ['rock', 'paper', 'scissors']
     let choice = choices[Math.floor(Math.random()*choices.length)];
     return choice
 }
 
 function playRound (cpuChoice, playerChoice) {
+    console.log(cpuChoice, playerChoice)
     if (cpuChoice == playerChoice) {
         console.log('DRAW')
+    } else if (cpuChoice == 'rock') {
+        if (playerChoice == 'paper') {
+            console.log('PAPER BEATS ROCK')
+        }
+        else if (playerChoice == 'scissors') {
+            console.log('ROCK BEATS SCISSORS')
+        }
+    } else if (cpuChoice == 'paper') {
+        if (playerChoice == 'rock') {
+            console.log('PAPER BEATS ROCK')
+        }
+        else if (playerChoice == 'scissors') {
+            console.log('SCISSORS BEATS PAPER')
+        } else if (cpuChoice == 'scissors') {
+        if (playerChoice == 'rock') {
+            console.log('ROCK BEATS SCISSORS')
+        } else if (playerChoice == 'paper') {
+            console.log('SCISSORS BEATS PAPER')
+        }
     }
-    else if (cpuChoice == 'rock' && playerChoice == 'paper')
+    }
 }
 
-console.log(getCpuChoice())
+playRound(computerChoice(), userChoice);
