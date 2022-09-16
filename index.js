a321ac0a0287ac6745782a4d1a9facfe8bd82661
MulfordnSons <mmulford94@me.com>
2022-09-16 19:32:07 -0400

    basic game logic completed

--- index.js
+++ index.js
@@ -57,16 +57,17 @@ startButton = document.getElementById('start-button');
 document.querySelectorAll('.button').forEach(item => {
     item.addEventListener('click', event => {
       console.log(`'button pressed ${item.textContent.toLowerCase()}'`)
-      round = playRound(computerChoice, item.textContent())
-      if (round == 'cpu') {
-        // insert the second value in array scores to div showing cpu score
-      }
+      round = playRound(computerChoice(), item.textContent.toLowerCase())
       
+        // insert the second value in array scores to div showing cpu score
+        document.getElementById('player-score').textContent = scores[0]
+        document.getElementById('cpu-score').textContent = scores[1]
+      })
+
       // add logic of game here, play a round and after each round, update
       // the scores in the bottom right of screen. Also, check the scores and once
       // a score reaches 5, end the game and display the winner.
     })
-  })
 
 // choices are disabled by default. when user clicks start, buttons are enabled and the
 // function that handles game logic is tied to the event listener for the choices.
